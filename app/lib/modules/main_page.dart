e0bbe10d58f2ba45856be500f7c8457e42201bf8
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-04-06 17:42:29 -0300

    refactoring

--- app/lib/modules/main_page.dart
+++ app/lib/modules/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:more_go_stuff/generated/todo/todo.pbgrpc.dart';
-import 'package:more_go_stuff/models/todo.dart';
+import 'package:more_go_stuff/widgets/create_todo_widget.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -12,78 +11,6 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  final _titleFocusNode = FocusNode();
-  final _contentFocusNode = FocusNode();
-
-  final _titleController = TextEditingController();
-  final _contentController = TextEditingController();
-
-  final _formKey = GlobalKey<FormState>();
-
-  bool _waiting = false;
-
-  Future<void> _createTodo() async {
-    if (_formKey.currentState?.validate() != true) return;
-
-    try {
-      setState(() {
-        _waiting = true;
-      });
-
-      final title = _titleController.text;
-      final content = _contentController.text;
-
-      final response = await Todo.addTodo(
-        AddTodoRequest()
-          ..title = title
-          ..content = content,
-      );
-
-      _titleController.clear();
-      _contentController.clear();
-
-      await showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Success'),
-            content: Text(response.message),
-            actions: [
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () async {
-                  await Navigator.of(context).maybePop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    } catch (e) {
-      await showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Warning'),
-            content: const Text('Something went wrong.'),
-            actions: [
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () async {
-                  await Navigator.of(context).maybePop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    } finally {
-      setState(() {
-        _waiting = false;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,68 +18,7 @@ class _MainPageState extends State<MainPage> {
       body: Center(
         child: ConstrainedBox(
           constraints: BoxConstraints.loose(const Size.fromWidth(768)),
-          child: Form(
-            key: _formKey,
-            child: ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 16,
-                    left: 16,
-                    right: 16,
-                  ),
-                  child: Text(
-                    'Create todo',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: TextFormField(
-                    enabled: !_waiting,
-                    focusNode: _titleFocusNode,
-                    controller: _titleController,
-                    decoration: const InputDecoration(
-                      label: Text('Title'),
-                      alignLabelWithHint: true,
-                    ),
-                    validator: (value) {
-                      value ??= '';
-
-                      if (value.length <= 4) return 'Title too short.';
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: TextFormField(
-                    enabled: !_waiting,
-                    focusNode: _contentFocusNode,
-                    controller: _contentController,
-                    decoration: const InputDecoration(
-                      label: Text('Content'),
-                      alignLabelWithHint: true,
-                    ),
-                    validator: (value) {
-                      value ??= '';
-
-                      if (value.length <= 4) return 'Content too short.';
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: ElevatedButton(
-                    onPressed: _waiting ? null : _createTodo,
-                    child: const Text(
-                      'CREATE TODO',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          child: const CreateTodoWidget(),
         ),
       ),
     );
---
+++ app/lib/widgets/create_todo_widget.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:more_go_stuff/generated/todo/todo.pb.dart';
+import 'package:more_go_stuff/models/todo.dart';
+
+class CreateTodoWidget extends StatefulWidget {
+  const CreateTodoWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CreateTodoWidget> createState() => _CreateTodoWidgetState();
+}
+
+class _CreateTodoWidgetState extends State<CreateTodoWidget> {
+  final _titleFocusNode = FocusNode();
+  final _contentFocusNode = FocusNode();
+
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  bool _waiting = false;
+
+  Future<void> _createTodo() async {
+    if (_formKey.currentState?.validate() != true) return;
+
+    try {
+      setState(() {
+        _waiting = true;
+      });
+
+      final title = _titleController.text;
+      final content = _contentController.text;
+
+      final response = await Todo.addTodo(
+        AddTodoRequest()
+          ..title = title
+          ..content = content,
+      );
+
+      _titleController.clear();
+      _contentController.clear();
+
+      await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Success'),
+            content: Text(response.message),
+            actions: [
+              TextButton(
+                child: const Text('OK'),
+                onPressed: () async {
+                  await Navigator.of(context).maybePop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } catch (e) {
+      await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Warning'),
+            content: const Text('Something went wrong.'),
+            actions: [
+              TextButton(
+                child: const Text('OK'),
+                onPressed: () async {
+                  await Navigator.of(context).maybePop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } finally {
+      setState(() {
+        _waiting = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 16,
+              left: 16,
+              right: 16,
+            ),
+            child: Text(
+              'Create todo',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextFormField(
+              enabled: !_waiting,
+              focusNode: _titleFocusNode,
+              controller: _titleController,
+              decoration: const InputDecoration(
+                label: Text('Title'),
+                alignLabelWithHint: true,
+              ),
+              validator: (value) {
+                value ??= '';
+
+                if (value.length <= 4) return 'Title too short.';
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextFormField(
+              enabled: !_waiting,
+              focusNode: _contentFocusNode,
+              controller: _contentController,
+              decoration: const InputDecoration(
+                label: Text('Content'),
+                alignLabelWithHint: true,
+              ),
+              validator: (value) {
+                value ??= '';
+
+                if (value.length <= 4) return 'Content too short.';
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: ElevatedButton(
+              onPressed: _waiting ? null : _createTodo,
+              child: const Text(
+                'CREATE TODO',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
